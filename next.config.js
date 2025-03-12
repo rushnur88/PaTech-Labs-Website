94ae1318c4dee0007b037ce8297db4c1ce423995
rushnur88 <ravshannuraliev@gmail.com>
2025-03-11 20:52:27 -0400

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'firebasestorage.googleapis.com',
+      'replicate.delivery',
+      'lh3.googleusercontent.com',
+    ],
+  },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { canvas: 'canvas' }];
+    return config;
+  },
+};
+
+module.exports = nextConfig;
